d2ac4ecab838f4fbead9b6616af539011fa273c6
SalmanStarneo <salman.alsamiri@gmail.com>
2022-03-26 18:04:05 +0000

    wk9 debugging

--- mchq/App.js
+++ mchq/App.js
@@ -68,23 +68,11 @@ const App: () => Node = () => {
         <Header />
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            backgroundColor: isDarkMode ? Colors.black : Colors.blue,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            MCHQ Mobile College Helper QR <Text style={styles.highlight}>App.js</Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-            Hello World !
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
