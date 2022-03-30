11636aee1d59010638d4aa5c20c1333152b96894
SalmanStarneo <salman.alsamiri@gmail.com>
2022-03-30 16:57:21 +0100

    wk10 updating navigation

--- mchq/App.js
+++ mchq/App.js
@@ -1,3 +1,6 @@
+// import * as React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
