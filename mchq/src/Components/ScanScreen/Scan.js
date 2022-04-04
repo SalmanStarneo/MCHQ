943e6ecd1a1a716d361148040503cc2d0b7b7651
SalmanStarneo <salman.alsamiri@gmail.com>
2022-04-04 18:37:54 +0100

    wk11 updating

--- mchq/src/Components/ScanScreen/Scan.js
+++ mchq/src/Components/ScanScreen/Scan.js
@@ -11,3 +11,28 @@ import { BarCodeScanner } from "expo-barcode-scanner";
 // }
 
 // const generatedQR = <img src="generateQR(${data })"/>;
+
+// const QRcode = require('qrcode');
+// const generateQR = async text => {
+
+//     try {
+//         console.log(await QRcode.toString(text,{type:'terminal'}));
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
+
+// generateQR("https://link.com/");
+
+//save as an image 
+// const QRcode = require('qrcode');
+// const generateQR = async text => {
+
+//     try {
+//         await QRcode.toFile('./img.png',text) ;
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
+
+// generateQR("https://link.com/");
