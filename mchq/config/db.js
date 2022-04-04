016d1602c0f672918594accc14a0d61a33695788
SalmanStarneo <salman.alsamiri@gmail.com>
2022-04-04 22:57:18 +0100

    wk11 updating dependencies

--- mchq/config/db.js
+++ mchq/config/db.js
@@ -1,2 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI,
+    {newURlParser: true,
+     useUnifiedTopology: true,}).then(() => {
+        console.log("DB Connected");
+     }).catch((err)=> console.log(err));
